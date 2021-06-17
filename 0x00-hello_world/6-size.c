28830f5fea7752cf044ea61a915f528593d38b3e
Kwendo-K <kevinkwendo95@gmail.com>
2021-06-17 13:21:18 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,9 +14,14 @@ int main(void)
 	float e;
 
 	printf("Size of char: %ld byte(s)\n", sizeof(a));
+
 	printf("Size of int: %ld bytes(s)\n", sizeof(b));
+
 	printf("Size of long int: %ld byte(s)\n", sizeof(c));
+
 	printf("Size of long long int %ld byte(s)\n", sizeof(d));
+
 	printf("Size of float: %ld byte(s)\n", sizeof(e));
+
 	return (0);
 }
