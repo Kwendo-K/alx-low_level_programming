4a292f7bc0fefb1e25d960e6bf990a0670d5f6a7
Kwendo-K <kevinkwendo95@gmail.com>
2021-06-18 06:38:18 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *
+ *this program prints numbers of base 10
+ *Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	printf("\n");
+	return (0);
+}
