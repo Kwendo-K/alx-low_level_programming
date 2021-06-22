440e685ca83686938bfc96a17d65ebac4f79cce8
Kwendo-K <kevinkwendo95@gmail.com>
2021-06-22 09:48:38 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,17 @@
 #include "holberton.h"
-/**
- *  * print_alphabet_x10 - function to print abc 10 times
- *   *
- *    * Return: 0
- *     */
-void print_alphabet_x10(void)
-{
-	char c, i;
 
-	for (i = 0; i <= 9; i++)
+/**
+ *main - check the code for ALX Scholl students.
+ *
+ *Return: 0.
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
 	{
-		for (c = 'a'; c <= 'z'; c++)
-			{
-				_putchar(c);
-			}
-			_putchar('\n');
+		_putchar(c);
 	}
+_putchar('\n');
 }
