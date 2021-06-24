fdea7a05b79771a3a3136f58bf522cff67a0d5a9
Kwendo-K <kevinkwendo95@gmail.com>
2021-06-24 15:04:48 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * main - a program that prints a straight line
+ *
+ * Return: 0
+ */
+void print_line(int n)
+{
+	for (n = '0'; n <= '9'; n++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
