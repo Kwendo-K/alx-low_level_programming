0f6a251c83f77051862c6acfc6470c37962a3451
Kwendo-K <kevinkwendo95@gmail.com>
2021-06-28 13:29:24 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * a function that swaps the values of two integers
+ * @a,b: parameter
+ * Description: swap the values of a & b
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
