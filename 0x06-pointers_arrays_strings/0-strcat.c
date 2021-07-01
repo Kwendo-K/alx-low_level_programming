73fd4083087ecba8df870f6c5805af60e49ae79f
Kwendo-K <kevinkwendo95@gmail.com>
2021-07-01 01:48:19 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * char *_strcat - concatinate two strings
+ *
+ * @*dest first parameter
+ * @*src second parammeter
+ * Description - a function that concatinates two strings
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return dest;
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory contains a 0x06. c-mre pointers, arrays and strings files
